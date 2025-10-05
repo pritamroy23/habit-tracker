f8b58cf62a00924f9fab53e517acf2c8c2ec0a46
Pritam Roy <roypritam3114@gmail.com>
2025-10-05 23:59:18 +0530

    header done

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 const header = ({ darkMode, toggleDarkMode }) => {
   return (
-    <header>
-      <h1>Habit Tracker</h1>
-      <button onClick={toggleDarkMode}>
-        {darkMode ? '☀️ Light Mode' : '🌙 Dark Mode'}
+    <header className='flex justify-between items-center mb-8 p-4 bg-white rounded-lg shadow-md'>
+      <h1 className="text-3xl font-bold text-blue-600 ">Habit Tracker</h1>
+      <button onClick={toggleDarkMode}
+      className ={`px-4 py-2 rounded-full font-semibold transition-all duration-300 ${
+        darkMode 
+        ?'bg-gray-300 hover:bg-gray-400'
+        : 'bg-gray-800  hover:bg-grey-900'
+      }`}>
+        
+        {darkMode ? '☀️ ' : '🌙 '}
       </button>
     </header>
   );
